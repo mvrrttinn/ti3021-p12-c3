d5f7162cca1da092440b6deb4bbfa57809736908
vttinn_ <martin.tapia16@inacapmail.cl>
2025-10-23 10:57:04 -0300

    yyeye

---
+++ Librerias/python_tkinter.py
@@ -0,0 +1,12 @@
+##OBLIGATORIO: Importar TKINTER
+from tkinter import * 
+from tkinter import ttk 
+
+##OBLIGATORIO: Inicializar TKINTER
+root: Tk = Tk()
+
+#Crear un lienzo. contexto, estilizacion.
+framel: Frame = ttk.Frame(root, padding=10 )
+
+#OBLIGATORIO: Mantener el cliclo de vida.
+root.mainloop()
