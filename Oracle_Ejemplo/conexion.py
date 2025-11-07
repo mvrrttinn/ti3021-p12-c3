fbfd07f413c045b4f1b5afb20ba66687833911c7
vttinn_ <martin.tapia16@inacapmail.cl>
2025-11-07 11:21:56 -0300

    Conexion con crud

--- Oracle_Ejemplo/conexion.py
+++ Oracle_Ejemplo/conexion.py
@@ -7,8 +7,13 @@ username = os.getenv("ORACLE_USER")
 dsn = os.getenv("ORACLE_DSN") 
 password = os.getenv("ORACLE_PASSWORD")  
 
-with oracledb.connect(user= username, password= password, dsn= dsn) as connection:
- with connection.cursor() as cursor:         
-        sql = "select sysdate from dual"         
-        for r in cursor.execute(sql):             
-         print(r) 
+with oracledb.connect(
+user= username, 
+password= password, 
+dsn= dsn
+) as connection:
+       with connection.cursor() as cursor:         
+              sql = "select sysdate from dual"         
+              for row in cursor.execute(sql):
+                     for column in row:             
+                            print(column) 
