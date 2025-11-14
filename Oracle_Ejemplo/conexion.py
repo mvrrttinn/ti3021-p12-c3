b116036681e46124ee39756a38a203628bc9edf1
vttinn_ <tapiajaram@gmail.com>
2025-11-14 11:04:28 -0300

    SCHEMA ORACLE
    schema oracledb para restaurante

--- Oracle_Ejemplo/conexion.py
+++ Oracle_Ejemplo/conexion.py
@@ -17,3 +17,76 @@ dsn= dsn
               for row in cursor.execute(sql):
                      for column in row:             
                             print(column) 
+
+def get_connection():
+       return oracledb.connect(iuser=username, password=password, dsn=dsn)                            
+
+def create_schema(query):
+       try:
+              with get_connection() as connection:
+                     with connection.cursor() as cursor:
+                            cursor.execute(query)
+                            print(f"Tabla creada \n {query}")
+       except oracledb.DatabaseError as error:
+              print(f"No se pudo crear la tabla: {error}")                     
+       
+       tables = [
+              (
+                     "CREATE TABLE"
+                     "CLIENTE ("
+                     "id INTERGER PRIMARY KEY,"
+                     "nombre VARCHAR(200),"
+                     "telefono VARCHAR(12)"
+                     ");"
+              ),       
+              (
+
+                     "CREATE TABLE"
+                     "RESTAURANTE ("
+                     "Direccion VARCHAR(30)"
+                     ");"
+              ),
+              (       
+
+                     "CREATE TABLE"
+                     "PEDIDO ("
+                     "id INTERGER PRIMARY KEY,"
+                     "fecha DATETIME,"
+                     "total INTERGER,"
+                     "id_clienteFK INTERGER,"
+                     "FOREIGN KEY id_clienteFK REFERENCES CLIENTE(id)"
+                     ");"
+              )
+              (       
+                     "CREATE TABLE"
+                     "PEDIDO_LOCAL ("
+                     "numeroMesa INTERGER"
+                     ");"
+              )
+              (
+                     "CREATE TABLE"
+                     "PEDIDO_DOMICILIO ("
+                     "direccionEntrega VARCHAR(100),"
+                     "nombreRepartidor VARCHAR(20)"
+                     "nombreReceptor VARCHAR(20)"
+                     ");"
+              )
+              (
+                     "CREATE TABLE"
+                     "PEDIDO_RETIRO ("
+                     "tiempoEstimadoRetiro TIME"
+                     ");"
+              )
+              (
+                     "CREATE TABLE"
+                     "BOLETA ("
+                     "fecha DATETIME,"
+                     "id_pedidoFK INTERGER,"
+                     "total INTERGER;"
+                     "FOREIGN KEY id_pedidoFK references PEDIDO(id)"
+                     ");"
+
+              )
+       ]
+for query in tables:
+       create_schema(query)       
